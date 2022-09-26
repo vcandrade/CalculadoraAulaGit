60808eae70c9d12dc028dfb0cca6ef69a05e66cb
Vinicius Camargo Andrade <vcandrade@utfpr.edu.br>
2022-09-26 10:57:05 -0300

    adicionado a funcionadade de divisao

--- src/service/Calculadora.java
+++ src/service/Calculadora.java
@@ -16,4 +16,9 @@ public class Calculadora {
 
 		System.out.println(x + " * " + y + " = " + (x * y));
 	}
+	
+	public void dividir(int x, int y) {
+
+		System.out.println(x + " / " + y + " = " + (x / y));
+	}
 }
--- src/service/CalculadoraTeste.java
+++ src/service/CalculadoraTeste.java
@@ -8,5 +8,6 @@ public class CalculadoraTeste {
 		calc.somar(10, 5);
 		calc.subtrair(10, 5);
 		calc.multiplicar(10, 5);
+		calc.dividir(10, 5);
 	}
 }
