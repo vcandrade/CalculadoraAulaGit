447b67d16587fbe2638bb5ee400dd8c6b765ab76
Vinicius Camargo Andrade <vcandrade@utfpr.edu.br>
2022-09-26 10:52:06 -0300

    adicionado as funcoes de somar e subtrair

---
+++ src/service/Calculadora.java
@@ -0,0 +1,14 @@
+package service;
+
+public class Calculadora {
+
+	public void somar(int x, int y) {
+
+		System.out.println(x + " + " + y + " = " + (x + y));
+	}
+
+	public void subtrair(int x, int y) {
+
+		System.out.println(x + " - " + y + " = " + (x - y));
+	}
+}
---
+++ src/service/CalculadoraTeste.java
@@ -0,0 +1,11 @@
+package service;
+
+public class CalculadoraTeste {
+
+	public static void main(String[] args) {
+		
+		Calculadora calc = new Calculadora();
+		calc.somar(10, 5);
+		calc.subtrair(10, 5);
+	}
+}
